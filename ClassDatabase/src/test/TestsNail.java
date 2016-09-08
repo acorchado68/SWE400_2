97f5768eee48b4b42be61dd4799048b2a5fc9523
Scott Bowling <sb6844@yahoo.com>
2016-09-07 20:11:34 -0400

    Attempting to test to the database

--- ClassDatabase/src/test/TestsNail.java
+++ ClassDatabase/src/test/TestsNail.java
@@ -1,19 +1,44 @@
 package test;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import src.*;
-import org.junit.Test;
-
-public class TestsNail extends abstractTests 
-{	
-	@Test
-	public void test() throws SQLException 
-	{
-		NailMapper example = new NailMapper(5, "absolute", 5, 10, 6, 5);
-		
-	
-	}
-
-}
+import static org.junit.Assert.*; 
+ 
+import java.sql.Connection; 
+import java.sql.PreparedStatement; 
+import java.sql.ResultSet; 
+import java.sql.ResultSetMetaData; 
+import java.sql.SQLException; 
+ 
+import org.junit.After; 
+import org.junit.Before; 
+ 
+import src.*; 
+ 
+import org.junit.Test; 
+ 
+public class TestsNail extends abstractTests  
+{
+  @Test 
+  public void test() throws SQLException  
+  { 
+    String [] Checker = {"5", "5"}; 
+    PreparedStatement stmt = null; 
+    NailMapper example = new NailMapper(5, "absolute", 5, 10, 6, 5); 
+    Connection conn = TestConnection.getConnection(); 
+    String query = "Select * From Nail"; 
+    stmt = conn.prepareStatement(query); 
+    ResultSet rs = stmt.executeQuery(); 
+    ResultSetMetaData rsmd = rs.getMetaData(); 
+    int incrementer = rsmd.getColumnCount(); 
+    int j = 0; 
+    while (rs.next())  
+    { 
+      for(int i = 1 ; i <= incrementer; i++) 
+      { 
+        System.out.print(rs.getString(i) + " "); 
+            
+      } 
+    } 
+  } 
+ 
+     
+} 
