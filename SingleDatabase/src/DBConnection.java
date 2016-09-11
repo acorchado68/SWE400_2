a7c843b0a8ebdbb527684f977e8568e055abcb03
Nick Martinez <habbarazzer@gmail.com>
2016-09-11 15:18:25 -0400

    adding DBConnection to the branch

---
+++ SingleDatabase/src/DBConnection.java
@@ -0,0 +1,53 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * Nick Martinez and Andrew Corchado - Single Table Inheritence
+ * Created by Nick Martinez on 9/5/16.
+ */
+public class DBConnection
+{
+    private static Connection conn;
+
+    private static boolean testMode = false;
+
+    public static Connection getConnection() throws SQLException
+    {
+        String url = "jdbc:mysql://157.160.36.32:3306/swe400-21?autoReconnect=true";
+        String username = "swe400_2";
+        String password = "pwd4swe400_2F16";
+
+        System.out.println("Connecting database...");
+
+        conn = DriverManager.getConnection(url, username, password);
+
+        if( testMode )
+        {
+            conn.setAutoCommit(false);
+        }
+
+        return conn;
+    }
+
+    /**
+     * Sets whether DBConnection should return a connection in "test mode" or not.
+     * @param state - true if in test mode, false if not to be in test mode
+     */
+    public static void setTestMode(boolean state)
+    {
+        testMode = state;
+    }
+
+    public static void main( String[] args ) {
+        DBConnection test = new DBConnection();
+        try {
+            Connection conn = DBConnection.getConnection();
+        } catch ( SQLException e ) {
+            System.out.println("uh-oh");
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ SingleDatabase/test/InheritableTest.java
@@ -0,0 +1,30 @@
+import org.junit.After;
+import org.junit.Before;
+
+import java.sql.Connection;
+
+/**
+ * Created by Andrew Corchado on 9/11/16.
+ */
+public abstract class InheritableTest
+{
+    /*
+     * Start testing.
+     */
+    @Before
+    public void before() throws Exception
+    {
+        DBConnection.setTestMode(true);
+    }
+
+    /*
+     * Close testing and rollback.
+     */
+    @After
+    public void after() throws Exception
+    {
+        Connection connection = DBConnection.getConnection();
+        connection.rollback();
+        DBConnection.setTestMode(false);
+    }
+}
