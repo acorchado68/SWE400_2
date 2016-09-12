95554f5842f73d379b9af23d285a4bfab5a640ae
mb8542 <mb8542@nahdudenah>
2016-09-12 03:31:22 -0400

    fixed TestSuite

--- ConcreteTableInheritance/src/domain/test/TestSuite.java
+++ ConcreteTableInheritance/src/domain/test/TestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PrimaryTest.class })
+@SuiteClasses({ PrimaryTest.class, TestInventoryItem.class })
 public class TestSuite {
 
 }
